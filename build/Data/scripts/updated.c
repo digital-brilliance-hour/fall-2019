9a97d7d199233bd9b59d65dd247a6369a4b19543
Digital Brilliance Hour <digitalbrilliancehour@gmail.com>
2019-11-16 19:54:46 -0500

    staging, scenes, .etc

--- build/Data/scripts/updated.c
+++ build/Data/scripts/updated.c
@@ -29,10 +29,63 @@ void mainLoop()
 			drawstring(124,212,0,"CRxTRDude 2014");
 		}*/
 	}
+
+	if(openborvariant("in_selectscreen")==1){
+      int models_cached = openborvariant("models_cached");
+      int i = 0;
+      int C = getglobalvar("Load");
+
+      for( i = 0; i < models_cached; ++i ) {
+        char model = getmodelproperty(i,2);
+
+        if( model == "Royce" && C != 39 ) {
+            changemodelproperty(i,4,0);
+            return;
+        }
+      }
+    } else if(openborvariant("in_menuscreen")==1){
+      setindexedvar(0, NULL());
+    }
 }
 
 void inLevelLoop()
 {
 	turnWhite();
 	storySystem();
+}
+
+void oncreate()
+{
+    int C = fileskim("Rise");
+    setglobalvar("Load", C);
+
+    setindexedvar(1, 0);
+}
+
+void fileskim(char Line)
+{
+      void file = openfilestream("Test.txt", 1);
+      void vLoad;
+
+      if(file != -1){
+        setfilestreamposition(file, 0);
+        vLoad = getfilestreamargument(file, 0, "string");
+
+        while(vLoad != Line && vLoad != "#End"){
+          filestreamnextline(file);
+          vLoad = getfilestreamargument(file, 0, "string");         
+        }
+        if( vLoad == Line ){
+          filestreamnextline(file);
+          vLoad = getfilestreamargument(file, 0, "int");
+          return vLoad;
+
+          closefilestream(file);
+        } else if(vLoad == "#End"){
+          closefilestream(file);
+          return 0;
+        }
+      } else {
+        return 0;
+      }
 }
